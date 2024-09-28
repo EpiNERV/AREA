bf1a415d5a68b5829079ff6e209f994e5425d9ba
Enzo Maltese <enzo.maltese@epitech.eu>
2024-09-28 21:08:56 +0200

    update: ci test coverage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/?(*.)+(test).ts'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    reporters: [
+        'default',
+        [
+          'jest-junit',
+          {
+            outputDirectory: './',
+            outputName: 'jest-results.xml',
+          },
+        ],
+    ],
+    coverageReporters: ['text', 'lcov'],
 };
