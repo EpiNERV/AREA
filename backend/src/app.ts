e69286ae5a39fd8e525c71f6b91b8edbc4473038
Enzo Maltese <enzo.maltese@epitech.eu>
2024-09-30 16:37:09 +0200

    fix: dockerfiles

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ export const connectDB = async (mongoUri?: string) => {
         : process.env.MONGO_URI ?? '';
 
     await mongoose.connect(mongoConnectionUri);
-    console.log('MongoDB connected to ${mongoConnectionUri}');
+    console.log('MongoDB connected');
   } catch (error: any) {
     console.error('MongoDB connection error:', error.message ?? 'Unknown error');
     process.exit(1);
