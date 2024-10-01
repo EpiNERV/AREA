8666ab6671b92ade40aa2de6a66ea70148a2f33b
lelouch178 <lelouch83@proton.me>
2024-10-01 14:13:39 +0200

    Fix: app.tsx function with theme provider, dark default theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import AppRoutes from './routes';
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 function App() {
 	return (
-		<>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 			<AppRoutes />
-		</>
+		</ThemeProvider>
 	);
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,8 +2,8 @@ const Home = () => {
 	return (
 		<div className="flex items-center justify-center h-screen">
 			<div className="text-center">
-				<h1 className="text-4xl font-bold text-black">Home Page</h1>
-				<p className="mt-4 text-lg text-black">Bienvenue sur la page d'accueil</p>
+				<h1 className="text-4xl font-bold">Home Page</h1>
+				<p className="mt-4 text-lg">Bienvenue sur la page d'accueil</p>
 			</div>
 		</div>
 	);
