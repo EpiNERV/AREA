26d7af2542dcc2170962f7a7924d6cf0644d0e53
lelouch178 <lelouch83@proton.me>
2024-10-01 10:57:56 +0200

    Add: all related login pages files

---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+	return (
+		<div>
+			<h1>Login Page</h1>
+			<p>Veuillez vous connecter</p>
+		</div>
+	);
+};
+
+export default Login;
---
+++ frontend/src/pages/login/PasswordForgotten.tsx
@@ -0,0 +1,10 @@
+function PasswordForgotten () {
+	return (
+		<div>
+			<h1>Mot de passe oublié</h1>
+			<p>Entrez votre adresse email pour recevoir un lien de réinitialisation de mot de passe.</p>
+		</div>
+	);
+}
+
+export default PasswordForgotten;
