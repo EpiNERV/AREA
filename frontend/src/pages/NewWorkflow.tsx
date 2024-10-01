82a53703c62b642f20839f4375e29c5f1f71b583
lelouch178 <lelouch83@proton.me>
2024-10-01 10:58:34 +0200

    Add: all related normal pages files

---
+++ frontend/src/pages/NewWorkflow.tsx
@@ -0,0 +1,12 @@
+const NewWorkflow = () => {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<div className="text-center">
+				<h1 className="text-4xl font-bold text-black">NewWorkflow Page</h1>
+				<p className="mt-4 text-lg text-black">Bienvenue sur la page NewWorkflow</p>
+			</div>
+		</div>
+	);
+};
+
+export default NewWorkflow;
