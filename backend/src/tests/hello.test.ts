8bccf000940d378b290db61cfedb029439379c19
Enzo Maltese <enzo.maltese@epitech.eu>
2024-10-01 09:45:50 +0200

    rename: backend test hello

--- backend/src/tests/hello.test.ts
+++ backend/src/tests/hello.test.ts
@@ -1,20 +1,6 @@
 import request from 'supertest';
 import app from '../app';
 
-import { connect, closeDatabase, clearDatabase } from './dbSetup';
-
-beforeAll(async () => {
-  await connect();
-});
-
-afterEach(async () => {
-  await clearDatabase();
-});
-
-afterAll(async () => {
-  await closeDatabase();
-});
-
 describe('GET /api/v1/hello', () => {
   it('should return a 200 status and a hello message', async () => {
     const response = await request(app).get('/api/v1/hello');
