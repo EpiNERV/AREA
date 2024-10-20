f99213388d73245bc1150681c529e7b946373391
Enzo Maltese <enzo.maltese@epitech.eu>
2024-10-20 16:04:07 +0200

    modify: better home page Add photos Better button

--- mobile/app/register.tsx
+++ mobile/app/register.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { HelloWave } from "@/components/HelloWave";
+import { View } from "react-native";
+import { Text, withTheme } from "react-native-paper";
+import { ThemeProp } from 'react-native-paper/lib/typescript/types';
 
-export default function Login() {
+const register = ({ theme }: Readonly<{ theme: ThemeProp }>) => {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: theme.colors ? theme.colors.background : undefined,
       }}
     >
-      <Text>Register page</Text>
+      <Text variant="displayMedium">Welcome <HelloWave /></Text>
     </View>
   );
 }
+
+export default withTheme(register);
