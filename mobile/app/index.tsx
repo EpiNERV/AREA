d99ddda5def7e04415ed20f9e5c9ab7da4166f4e
Enzo Maltese <enzo.maltese@epitech.eu>
2024-10-20 16:04:07 +0200

    modify: better home page Add photos Better button

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,38 +1,49 @@
-import { SafeAreaView, View } from "react-native";
-import { withTheme, Text, Button} from "react-native-paper";
+import { View, Image, StyleProp, ViewStyle, TextStyle } from "react-native";
+import { withTheme, Button} from "react-native-paper";
 import { ThemeProp } from 'react-native-paper/lib/typescript/types';
-import { Link, useNavigation } from 'expo-router';
-import { useEffect } from 'react';
+import { Link } from 'expo-router';
+import Spacer from "@/components/Spacer";
 
 const Home = ({ theme }: Readonly<{ theme: ThemeProp }>) => {
-  const navigation = useNavigation();
-
-  // useEffect(() => {
-  //   navigation.setOptions({ headerShown: false });
-  // }, [navigation]);
-
-
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         backgroundColor: theme.colors ? theme.colors.background : undefined,
       }}>
       <View
         style={{
           alignItems: "center",
         }}>
-        <Text>Welcome</Text>
+        <Spacer size={50} />
+        <Image style={{ height: 300}} resizeMode="contain" source={require("@/assets/images/welcome.png")}></Image>
+        <Spacer size={75} />
         <Link href="/login" asChild>
-          <Button mode="contained">Login</Button>
+          <Button mode="contained" contentStyle={buttonContentStyle} labelStyle={buttonTextStyle} style={buttonStyle}>Login</Button>
         </Link>
+        <Spacer />
         <Link href="/register" asChild>
-          <Button mode="contained">Register</Button>
+          <Button mode="contained" contentStyle={buttonContentStyle} labelStyle={buttonTextStyle} style={buttonStyle}>Register</Button>
         </Link>
       </View>
     </View>
   );
-}
+};
+
+const buttonStyle: StyleProp<ViewStyle> = {
+  width: '80%',
+  marginVertical: 5,
+};
+
+const buttonContentStyle: StyleProp<ViewStyle> = {
+  flexDirection: "row-reverse",
+  paddingVertical: 6,
+  paddingHorizontal: 16,
+  marginVertical: 5,
+};
+
+const buttonTextStyle: StyleProp<TextStyle> = {
+  fontSize: 18,
+};
 
 export default withTheme(Home)
---
+++ mobile/components/HelloWave.tsx
@@ -0,0 +1,27 @@
+import { Text } from 'react-native-paper';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+  withSequence,
+} from 'react-native-reanimated';
+
+export function HelloWave() {
+  const rotationAnimation = useSharedValue(0);
+
+  rotationAnimation.value = withRepeat(
+    withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
+    4 // Run the animation 4 times
+  );
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotationAnimation.value}deg` }],
+  }));
+
+  return (
+    <Animated.View style={animatedStyle}>
+      <Text variant="displaySmall">👋</Text>
+    </Animated.View>
+  );
+}
