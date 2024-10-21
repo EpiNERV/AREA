ff0e8430995448e2b9af56eaaab591d8bb2b2691
lelouch178 <lelouch83@proton.me>
2024-10-21 23:46:52 +0200

    Add: profileInfos & profileServices page

---
+++ frontend/src/pages/ProfileInfos.tsx
@@ -0,0 +1,12 @@
+const ProfileInfos = () => {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<div className="text-center">
+				<h1 className="text-4xl font-bold">Profile informations Page</h1>
+				<p className="mt-4 text-lg">Welcome to ProfileInfos</p>
+			</div>
+		</div>
+	);
+};
+
+export default ProfileInfos;
---
+++ frontend/src/pages/ProfileServices.tsx
@@ -0,0 +1,12 @@
+const ProfileServices = () => {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<div className="text-center">
+				<h1 className="text-4xl font-bold">Profile services integrations Page</h1>
+				<p className="mt-4 text-lg">Welcome to ProfileServices</p>
+			</div>
+		</div>
+	);
+};
+
+export default ProfileServices;
