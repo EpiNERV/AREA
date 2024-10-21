bcb6cabc8a7446f40a33ceeb88924a4dddb9d416
lelouch178 <lelouch83@proton.me>
2024-10-22 00:46:59 +0200

    Add: new profile Informations page
    no API logic implemented
    only UI

--- frontend/src/pages/ProfileInfos.tsx
+++ frontend/src/pages/ProfileInfos.tsx
@@ -1,9 +1,88 @@
+import { Card,  CardContent,  CardDescription,  CardFooter,  CardHeader,  CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 const ProfileInfos = () => {
 	return (
-		<div className="flex items-center justify-center h-screen">
-			<div className="text-center">
-				<h1 className="text-4xl font-bold">Profile informations Page</h1>
-				<p className="mt-4 text-lg">Welcome to ProfileInfos</p>
+		<div className="flex flex-1 items-center justify-center w-full h-full">
+			<div className="overflow-hidden">
+				<div className="p-6 grid grid-cols-3 grid-rows-2 gap-6">
+					{/* Username Form */}
+					<Card className="w-[350px]">
+						<CardHeader>
+							<CardTitle>Change username</CardTitle>
+							<CardDescription>Change your username within this card.</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<form>
+								<div className="grid w-full items-center gap-4">
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">Current username</Label>
+										<Input id="name" disabled placeholder="user current username"/>
+									</div>
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">New username</Label>
+										<Input id="name" placeholder="user new username"/>
+									</div>
+								</div>
+							</form>
+						</CardContent>
+						<CardFooter className="flex justify-center">
+							<Button>Change</Button>
+						</CardFooter>
+					</Card>
+
+					{/* Email Form */}
+					<Card className="w-[350px]">
+						<CardHeader>
+							<CardTitle>Change your email</CardTitle>
+							<CardDescription>Change your email within this card.</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<form>
+								<div className="grid w-full items-center gap-4">
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">Current email</Label>
+										<Input id="name" placeholder="Enter your current email"/>
+									</div>
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">New email</Label>
+										<Input id="name" placeholder="Enter your new email"/>
+									</div>
+								</div>
+							</form>
+						</CardContent>
+						<CardFooter className="flex justify-center">
+							<Button>Change</Button>
+						</CardFooter>
+					</Card>
+
+					{/* Password Form */}
+					<Card className="w-[350px]">
+						<CardHeader>
+							<CardTitle>Change your password</CardTitle>
+							<CardDescription>Change your password within this card.</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<form>
+								<div className="grid w-full items-center gap-4">
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">Password</Label>
+										<Input id="name"  placeholder="Enter your current password"/>
+									</div>
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">New password</Label>
+										<Input id="name" placeholder="Enter your new password"/>
+									</div>
+								</div>
+							</form>
+						</CardContent>
+						<CardFooter className="flex justify-center">
+							<Button>Change</Button>
+						</CardFooter>
+					</Card>
+				</div>
 			</div>
 		</div>
 	);
