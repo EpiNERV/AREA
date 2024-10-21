31e64f38e11aeaf95fdcbaf224584d3f724864c7
Enzo Maltese <enzo.maltese@epitech.eu>
2024-10-21 14:13:23 +0200

    modify: app layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,33 +1,13 @@
-import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  PaperProvider,
-} from 'react-native-paper';
-import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
-import { useColorScheme } from 'react-native';
-import { ThemeProp } from 'react-native-paper/lib/typescript/types';
+import React from 'react'
 import { Stack } from 'expo-router';
-import CustomAppBar from '@/components/CustomAppBar';
+import { PaperProvider } from 'react-native-paper';
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const { theme } = useMaterial3Theme();
-
-  const paperTheme: ThemeProp =
-  colorScheme === 'dark'
-    ? { ...MD3DarkTheme, colors: theme.dark }
-    : { ...MD3LightTheme, colors: theme.light };
-
-  return (
-    <PaperProvider theme={paperTheme}>
-      <Stack
-        screenOptions={{
-          header: (props) => <CustomAppBar {...props} />,
-        }}
-      >
-        <Stack.Screen name="login" options={{ title: "Login"}} />
-        <Stack.Screen name="register" options={{ title: "Register"}} />
-      </Stack>
-    </PaperProvider>
-  );
+export default function _layout() {
+    return (
+        <PaperProvider>
+            <Stack screenOptions={{ animation: 'none', headerShown: false}} >
+                <Stack.Screen options={{headerShown: false}} name="welcome"/>
+            </Stack>
+        </PaperProvider>
+    );
 }
