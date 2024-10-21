3fb36bab528bc2885252c98c15ab9a9b8f4e65b2
lelouch178 <lelouch83@proton.me>
2024-10-22 00:26:22 +0200

    Del: profile page before rework

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -19,7 +19,6 @@ import Accessibility from './pages/Accessibility.tsx';
 import NewWorkflow  from "./pages/NewWorkflow";
 
 // profile pages import
-import Profile from "./pages/Profile";
 import ProfileInfos from "@/pages/ProfileInfos.tsx";
 import ProfileServices from "@/pages/ProfileServices.tsx";
 
@@ -49,7 +48,6 @@ function AppRoutes() {
 						<Route path="/accessibility" element={<ProtectedRoute><Accessibility /></ProtectedRoute>} />
 			
 						{/* Profile */}
-						<Route path={"/user/profile"} element={<ProtectedRoute><Profile /></ProtectedRoute>} />
 						<Route path={"/user/profile_informations"} element={<ProtectedRoute><ProfileInfos /></ProtectedRoute>} />
 						<Route path={"/user/profile_services"} element={<ProtectedRoute><ProfileServices /></ProtectedRoute>} />
 			
