ea1e23d589131b04c393a3bab205bc1dc35c1706
lelouch178 <lelouch83@proton.me>
2024-10-22 18:15:03 +0200

    Fix: don't show sidebar when logged out

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/sidebar.tsx"
+import { useLocation } from "react-router-dom";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+	const location = useLocation();
+	const noNavBarLocations = ['/', '/login', '/register', '/password_forgotten', '/auth_verification', '/password_changed'];
+	const showNavBar = !noNavBarLocations.includes(location.pathname);
+
 	return (
 		<SidebarProvider>
-			<AppSidebar />
-			<main className="flex-1 flex flex-col">
-				<SidebarTrigger />
-				{children}
-			</main>
+			<div className="flex flex-1">
+				{showNavBar && <AppSidebar />}
+				<main className="flex-1 flex flex-col">
+					{showNavBar && <SidebarTrigger />}
+					{children}
+				</main>
+			</div>
 		</SidebarProvider>
-	)
+	);
 }
