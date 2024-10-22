9e73ed0d1cdb276dd64804694d6d1e3a2a1eee62
lelouch178 <lelouch83@proton.me>
2024-10-22 18:15:29 +0200

    fix: layout name variable

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,15 +5,15 @@ import { useLocation } from "react-router-dom";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	const location = useLocation();
-	const noNavBarLocations = ['/', '/login', '/register', '/password_forgotten', '/auth_verification', '/password_changed'];
-	const showNavBar = !noNavBarLocations.includes(location.pathname);
+	const noSideBarLocations = ['/', '/login', '/register', '/password_forgotten', '/auth_verification', '/password_changed'];
+	const showSideBade = !noSideBarLocations.includes(location.pathname);
 
 	return (
 		<SidebarProvider>
 			<div className="flex flex-1">
-				{showNavBar && <AppSidebar />}
+				{showSideBade && <AppSidebar />}
 				<main className="flex-1 flex flex-col">
-					{showNavBar && <SidebarTrigger />}
+					{showSideBade && <SidebarTrigger />}
 					{children}
 				</main>
 			</div>
